cc7e5c254c75d64a987fa3000248a4ea63804e7c
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-04 17:11:59 -0700

    Assembled functions into Calculator

--- calculator.py
+++ calculator.py
@@ -8,14 +8,48 @@ from arithmetic import *
 
 
 def operation():
-    inputs = raw_input('')
+    inputs = raw_input("")
     calc_data = inputs.split()
     return calc_data
 
 
-def calcing(calc_data):
-    if calc_data[0] == "+":
+def calcing():
+    calc_data = operation()
+    if calc_data == ['q'] or calc_data == ["quit"]:
+        return
+    elif calc_data[0] == "+":
         print add(int(calc_data[1]), int(calc_data[2]))
+        return calcing()
+    elif calc_data[0] == "-":
+        print subtract(int(calc_data[1]), int(calc_data[2]))
+        return calcing()
+    elif calc_data[0] == "*":
+        print multiply(int(calc_data[1]), int(calc_data[2]))
+        return calcing()
+    elif calc_data[0] == "/":
+        print divide(int(calc_data[1]), int(calc_data[2]))
+        return calcing()
+    elif calc_data[0] == "square":
+        print square(int(calc_data[1]))
+        return calcing()
+    elif calc_data[0] == "cube":
+        print cube(int(calc_data[1]))
+        return calcing()
+    elif calc_data[0] == "power":
+        print power(int(calc_data[1]), int(calc_data[2]))
+        return calcing()
+    elif calc_data[0] == "mod":
+        print mod(int(calc_data[1]), int(calc_data[2]))
+        return calcing()
+    elif calc_data[0] == "add_mult":
+        print add_mult(int(calc_data[1]), int(calc_data[2]), int(calc_data[3]))
+        return calcing()
+    elif calc_data[0] == "add_cubes":
+        print add_cubes(int(calc_data[1]), int(calc_data[2]))
+        return calcing()
+    else:
+        print ("Input is in an incorrect format. Use a math prefix followed by 2 numbers; each should be separated by a blank space.")
+        return calcing()
 
 
-calcing(operation())
+calcing()
